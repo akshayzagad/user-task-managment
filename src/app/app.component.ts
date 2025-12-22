9959ff3f4a5fd126c0c17a39c954ac6eb9f84b70
akshayzagad <akshayzagade62@gmail.com>
2025-12-22 21:41:33 +0530

    feat: add TaskComponent and integrate it into the main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,28 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-first-app';
   user = DUMMY_USERS;
+  task = DUMMY_USERS;
+
+  /** create property to store id which is emit bu user component and it temprary initilize */
+  selectedId = "u1";
+
+  get SelectedUser(){
+    return this.user.find((user)=>user.id === this.selectedId)!;
+  }
 
   onSelectUser(id:string){
     console.log(`Selected user with id` + id);
+    this.selectedId = id;
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -19,9 +19,9 @@ export class UserComponent {
 @Input({required:true}) id!: string;
 @Input({required:true}) avatar!: string;
 @Input({required:true}) name!: string;
-// @Output() select = new EventEmitter();
+@Output() select = new EventEmitter();
 
-select = output<string>();
+// select = output<string>();
 
 get imagePath(){
   return 'public/' + this.avatar
