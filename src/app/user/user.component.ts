1b7ea67235c158e75fde588b49cefe038521b349
akshayzagad <akshayzagade62@gmail.com>
2025-12-22 21:07:51 +0530

    use new output select syntax to emit an event

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, EventEmitter, input, Input, Output } from '@angular/core';
+import { Component, computed, EventEmitter, input, Input, output, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 
@@ -19,7 +19,9 @@ export class UserComponent {
 @Input({required:true}) id!: string;
 @Input({required:true}) avatar!: string;
 @Input({required:true}) name!: string;
-@Output() select = new EventEmitter();
+// @Output() select = new EventEmitter();
+
+select = output<string>();
 
 get imagePath(){
   return 'public/' + this.avatar
