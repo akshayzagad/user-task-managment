43da8fef4fc83524dfeb6d8f789eb7140e275c17
akshayzagad <akshayzagade62@gmail.com.com>
2025-12-26 21:34:24 +0530

    adding dynamic user using legasy code like ngIf and ngfor to render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
-import { TaskComponent } from "./task/task.component";
-import {dummyTasks} from "./dummy-tasks"
+import { TaskComponent } from './task/task.component';
+import { dummyTasks } from './dummy-tasks';
+import {
+  NgForOf,
+  NgIf,
+} from '../../node_modules/@angular/common/common_module.d-NEF7UaHr';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TaskComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    UserComponent,
+    TaskComponent,
+    NgForOf,
+    NgIf,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-first-app';
@@ -18,13 +29,13 @@ export class AppComponent {
   task = dummyTasks;
 
   /** create property to store id which is emit bu user component and it temprary initilize */
-  selectedId ?: string;
+  selectedId?: string;
 
-  get SelectedUser(){
-    return this.user.find((user)=>user.id === this.selectedId)!;
+  get SelectedUser() {
+    return this.user.find((user) => user.id === this.selectedId)!;
   }
 
-  onSelectUser(id:string){
+  onSelectUser(id: string) {
     console.log(`Selected user with id` + id);
     this.selectedId = id;
   }
